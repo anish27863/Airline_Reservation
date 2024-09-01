6863262a155c4293bda775d83aac5890aa2ae3c2
anish27863 <anish27863@gmail.com>
2024-09-01 23:34:05 +0530

    Removing the login function and instead creating sign-in and sign-up functions

--- Airline Reservation.py
+++ Airline Reservation.py
@@ -8,18 +8,23 @@ tickets={}
 
 def display_menu():
   """Prints the menu options for the airline reservation system."""
-  print("\nAirline Reservation System")
-  print("-" * 30)
-  print("1. Search available planes")
+  print("1. Search available planes") #delete this later
   print("2. Check seat availability")
   print("3. Book ticket")
   print("4. Cancel ticket")
   print("5. Check PNR status")
   print("6. View tickets")
   print("7. Exit")
-  print("8.login")
   print("\nPlease enter your choice (1-7): ")
 
+
+def initial_display_menu():
+  print("\nAirline Reservation System")
+  print("-" * 30)
+  print("1.Sign in ")
+  print("2.Sign up ")
+  print("3.Search planes ")
+  print("\nPlease enter your choice (1-3): ")
 
 def exitf():
   print("Exiting the system...")
@@ -32,17 +37,6 @@ def planes_available():
   cursor.execute('SELECT * FROM planes')
   planes=cursor.fetchall()
   print(pd.DataFrame.from_records(planes).reset_index(drop=True))
-
-def login():
-  uid=input("Enter username ")
-  name=input("Enter your name ")
-  email=input("Enter email address ")
-  ph=int(input("Enter phone number "))
-  passw=input("Create password")
-  sql="INSERT INTO users(username,Full_Name,email,phone,passw) VALUES(%s,%s,%s,%s,%s)"
-  values=(uid,name,email,ph,passw)
-  cursor.execute(sql,values)
-  con.commit()
 
 def seat_availability():
   '''checks seat availablilty in the selected Plane'''
@@ -163,38 +157,84 @@ def display_booked_tickets():
     '''displays booksed tickets'''
     print(print(pd.DataFrame.from_dict(tickets, orient='index').reset_index(drop=True)) )           
 
+def sign_up():
+  uid=input("Enter username ")
+  name=input("Enter your name ")
+  email=input("Enter email address ")
+  ph=int(input("Enter phone number "))
+  passw=input("Create password")
+  sql="INSERT INTO users(username,Full_Name,email,phone,passw) VALUES(%s,%s,%s,%s,%s)"
+  values=(uid,name,email,ph,passw)
+  cursor.execute(sql,values)
+  con.commit()
+
+  print("Now login using your details: ")
+  sign_in()
+
+def sign_in():
+  uid=input("Enter username: ")
+  passw=input("Enter password: ")
+  data=(uid,passw)
+  flag=True
+  global c
+  c=0
+  while flag:
+    cursor.execute("SELECT username,passw from users")
+    database_data=cursor.fetchone()
+    if(database_data==data):
+      flag=False
+      print('Welcome: ')
+      c=1
+      display_menu()
+    else:
+      print("Wrong details ")
+      print("Do you want to retry, enter yes or no")
+      ret=input()
+      if ret.lower=='no' :
+        exitf()  
+
+def execution():
+  initial_display_menu()
+  choice=int(input())
+  if(choice==1):
+    sign_in()
+  elif choice==2:
+    sign_up()
+  elif choice==3: 
+    planes_available()#replace it with plane search functionality          
 
 def main():
-  while True:
-    display_menu()
-    choice = int(input())
-    if choice == 1:
-      # Implement Plane search functionality
-      planes_available()
-      pass
-    elif choice == 2:
-      seat_availability()
-      # Implement seat availability check functionality
-      pass
-    elif choice == 3:
-        ticketbooking() # Implement ticket booking functionality
-    elif choice == 4:
-      cancel_ticket()# Implement ticket cancellation functionality
-      pass
-    elif choice == 5:
-      pnr_status()# Implement PNR status check functionality
-      pass
-    elif choice == 6:
-      display_booked_tickets()# Implement view Fare= tickets functionality
-      pass
-    elif choice == 7:
-      exitf()
-      break
-    elif choice == 8:
-      login()
-      pass
-    else:
-      print("Invalid choice. Please try again.")
+  execution()
+  if c==1:
+    while True:
+      display_menu()
+      choice = int(input())
+      if choice == 1:
+        # Implement Plane search functionality
+        planes_available()
+        pass
+      elif choice == 2:
+        seat_availability()
+        # Implement seat availability check functionality
+        pass
+      elif choice == 3:
+          ticketbooking() # Implement ticket booking functionality
+      elif choice == 4:
+        cancel_ticket()# Implement ticket cancellation functionality
+        pass
+      elif choice == 5:
+        pnr_status()# Implement PNR status check functionality
+        pass
+      elif choice == 6:
+        display_booked_tickets()# Implement view Fare= tickets functionality
+        pass
+      elif choice == 7:
+        exitf()
+        break
+      else:
+        print("Invalid choice. Please try again.")
+  else:
+    print("please sign_in ")
+    sign_in()  
 
-if __name__ == "__main__":
-  main()
+main()        
