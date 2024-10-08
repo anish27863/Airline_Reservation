277ee32325e337d597da98d0764ca550e665a6d6
anish27863 <anish27863@gmail.com>
2024-10-08 16:13:38 +0530

    setting all the data types to be varchar

--- tickets.sql
+++ tickets.sql
@@ -3,8 +3,8 @@ use airline
 CREATE TABLE tickets (
     username VARCHAR(50),
     plane_number VARCHAR(20),
-    booking_no INT PRIMARY KEY,
-    Passenger_Name CHAR(20),
+    booking_no VARCHAR(20) PRIMARY KEY,
+    Passenger_Name VARCHAR(20),
     classs VARCHAR(20),
     fare INT
 );
